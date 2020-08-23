919e352079122ec745c68ac1f92a5cfdf1d1e91f
Gustavo <gustavodiazarroyo@gmail.com>
2020-08-23 14:30:04 -0500

    Primer Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicacion de comandos
+
+Este es el ejercicio del curso de Node
+
+Instalar los paquetes de node
+````
+npm install
+````
---
+++ config/yargs.js
@@ -0,0 +1,34 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de tarea por acer'
+
+};
+
+const completado = {
+    demand: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+
+
+};
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra la tarea especificada', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
